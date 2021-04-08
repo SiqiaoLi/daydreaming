d50794e3005174ba21e108a14ad875933410ba30
Siqiao Li <11291141bjtu@gmail.com>
2021-04-08 11:31:38 +0800

    add dist to gitignore

--- dist/components/Button/Button.d.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-export interface IButtonProps {
-    children?: React.ReactNode;
-    onClick?: (e: any) => void;
-}
-export declare const Button: React.FunctionComponent<IButtonProps>;
